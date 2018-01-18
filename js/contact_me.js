4782210f3f5433732eb45a361be46cfeb05d5907
Finleth <jeffrey@paufamily.com>
2018-01-18 15:35:56 -0800

    set up initial php mailer and routed the front end to communicate with it

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "././mail/contact_me.php",
+        url: "././php_mailer/mail_handler.php",
         type: "POST",
         data: {
           name: name,
@@ -29,17 +29,21 @@ $(function() {
           message: message
         },
         cache: false,
-        success: function() {
+        success: function(res) {
           // Success message
-          $('#success').html("<div class='alert alert-success'>");
-          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
-          $('#success > .alert-success')
-            .append('</div>');
-          //clear all fields
-          $('#contactForm').trigger("reset");
+          if (res.success){
+            $('#success').html("<div class='alert alert-success'>");
+            $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+              .append("</button>");
+            $('#success > .alert-success')
+              .append("<strong>Your message has been sent. </strong>");
+            $('#success > .alert-success')
+              .append('</div>');
+            //clear all fields
+            $('#contactForm').trigger("reset");
+          } else {
+            console.log(res);
+          }
         },
         error: function() {
           // Fail message
---
+++ php_mailer/email_config.default.php
@@ -0,0 +1,6 @@
+<?php
+define('EMAIL_USER','<YOUR EMAIL>');
+define('EMAIL_TO_USER', '<TO USER EMAIL>');
+define('EMAIL_USERNAME', '<YOUR USERNAME>');
+define('EMAIL_PASS','<YOUR PASSWORD>');
+?>
